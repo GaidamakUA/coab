5155c00887cc3742f18790c5bdc84342db8bdd16
gaidmakua <jmazurevich@gmail.com>
2019-05-22 22:55:30 +0300

    Clean up and changed rendering to draw into GraphicsObject instead of replacing image.

--- Classes/Display.cs
+++ Classes/Display.cs
@@ -1,112 +1,111 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Drawing;
 using System.Drawing.Imaging;
 
-
 namespace Classes
 {
-    public interface IOSDisplay
-    {
-        void Init(int height, int width);
-        void RawCopy(byte[] videoRam, int videoRamSize);
-    }
-
     public enum TextRegion
     {
         NormalBottom,
         Normal2,
-        CombatSummary,
+        CombatSummary
     }
 
-    public class Display
+    public static class Display
     {
-        static byte[,] OrigEgaColors = { { 0, 0, 0 }, { 0, 0, 173 }, { 0, 173, 0 }, { 0, 173, 173 }, { 173, 0, 0 }, { 173, 0, 173 }, { 173, 82, 0 }, { 173, 173, 173 }, { 82, 82, 82 }, { 82, 82, 255 }, { 82, 255, 82 }, { 82, 255, 255 }, { 255, 82, 82 }, { 255, 82, 255 }, { 255, 255, 82 }, { 255, 255, 255 } };
-        static byte[,] egaColors = { { 0, 0, 0 }, { 0, 0, 173 }, { 0, 173, 0 }, { 0, 173, 173 }, { 173, 0, 0 }, { 173, 0, 173 }, { 173, 82, 0 }, { 173, 173, 173 }, { 82, 82, 82 }, { 82, 82, 255 }, { 82, 255, 82 }, { 82, 255, 255 }, { 255, 82, 82 }, { 255, 82, 255 }, { 255, 255, 82 }, { 255, 255, 255 } };
-        static int[,] ram;
-        static byte[] videoRam;
-        static byte[] videoRamBkUp;
-        static int videoRamSize;
-        static int scanLineWidth;
-        static int outputWidth;
-        static int outputHeight;
-        
-        static public Bitmap bm;
-        static Rectangle rect = new Rectangle(0, 0, 320, 200);
-
-        public delegate void VoidDeledate();
-
-        static VoidDeledate updateCallback;
-
-        static public VoidDeledate UpdateCallback
+        private static readonly byte[,] OrigEgaColors =
         {
-            set
-            {
-                updateCallback = value;
-            }
+            {0, 0, 0}, {0, 0, 173}, {0, 173, 0}, {0, 173, 173}, {173, 0, 0}, {173, 0, 173}, {173, 82, 0},
+            {173, 173, 173}, {82, 82, 82}, {82, 82, 255}, {82, 255, 82}, {82, 255, 255}, {255, 82, 82}, {255, 82, 255},
+            {255, 255, 82}, {255, 255, 255}
+        };
+
+        private static readonly byte[,] EgaColors =
+        {
+            {0, 0, 0}, {0, 0, 173}, {0, 173, 0}, {0, 173, 173}, {173, 0, 0}, {173, 0, 173}, {173, 82, 0},
+            {173, 173, 173}, {82, 82, 82}, {82, 82, 255}, {82, 255, 82}, {82, 255, 255}, {255, 82, 82}, {255, 82, 255},
+            {255, 255, 82}, {255, 255, 255}
+        };
+
+        private static readonly int[,] Ram;
+        private static byte[] _videoRam;
+        private static byte[] _videoRamBkUp;
+        private static readonly int VideoRamSize;
+        private static readonly int ScanLineWidth;
+        private static readonly int OutputWidth;
+        private static readonly int OutputHeight;
+
+        public static readonly Bitmap Bitmap;
+        private static readonly Rectangle Rect = new Rectangle(0, 0, 320, 200);
+
+        public delegate void VoidDelegate();
+
+        private static VoidDelegate _updateCallback;
+
+        public static VoidDelegate UpdateCallback
+        {
+            set => _updateCallback = value;
         }
 
         static Display()
         {
-            outputHeight = 200;
-            outputWidth = 320;
+            OutputHeight = 200;
+            OutputWidth = 320;
 
-            ram = new int[outputHeight, outputWidth];
-            scanLineWidth = outputWidth * 3;
-            videoRamSize = scanLineWidth * outputHeight;
-            videoRam = new byte[videoRamSize];
+            Ram = new int[OutputHeight, OutputWidth];
+            ScanLineWidth = OutputWidth * 3;
+            VideoRamSize = ScanLineWidth * OutputHeight;
+            _videoRam = new byte[VideoRamSize];
 
-            bm = new Bitmap(outputWidth, outputHeight, PixelFormat.Format24bppRgb);
+            Bitmap = new Bitmap(OutputWidth, OutputHeight, PixelFormat.Format24bppRgb);
         }
 
-        static int[] MonoBitMask = { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01 };
+        private static readonly int[] MonoBitMask = {0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
 
         public static void DisplayMono8x8(int xCol, int yCol, byte[] monoData8x8, int bgColor, int fgColor)
         {
-            int pX = xCol * 8;
+            var pX = xCol * 8;
 
-            for (int yStep = 0; yStep < 8; yStep++)
+            for (var yStep = 0; yStep < 8; yStep++)
             {
-                int pY = (yCol * 8) + yStep;
+                var pY = (yCol * 8) + yStep;
                 int value = gbl.monoCharData[yStep];
 
                 for (int i = 0; i < 8; i++)
                 {
-                    ram[pY, pX + i] = (value & MonoBitMask[i]) != 0 ? fgColor : bgColor;
-                    SetVidPixel(pX + i, pY, ram[pY, pX + i]);
+                    Ram[pY, pX + i] = (value & MonoBitMask[i]) != 0 ? fgColor : bgColor;
+                    SetVidPixel(pX + i, pY, Ram[pY, pX + i]);
                 }
             }
         }
 
         public static void SetEgaPalette(int index, int colour)
         {
-            egaColors[index, 0] = OrigEgaColors[colour, 0];
-            egaColors[index, 1] = OrigEgaColors[colour, 1];
-            egaColors[index, 2] = OrigEgaColors[colour, 2];
+            EgaColors[index, 0] = OrigEgaColors[colour, 0];
+            EgaColors[index, 1] = OrigEgaColors[colour, 1];
+            EgaColors[index, 2] = OrigEgaColors[colour, 2];
 
-            for (int y = 0; y < outputHeight; y++)
+            for (var y = 0; y < OutputHeight; y++)
             {
-                int vy = y * scanLineWidth;
-                for (int x = 0; x < outputWidth; x++)
+                var vy = y * ScanLineWidth;
+                for (var x = 0; x < OutputWidth; x++)
                 {
-                    int vx = x * 3;
-                    int egaColor = ram[y, x];
+                    var vx = x * 3;
+                    var egaColor = Ram[y, x];
 
-                    videoRam[vy + vx + 0] = egaColors[egaColor, 2];
-                    videoRam[vy + vx + 1] = egaColors[egaColor, 1];
-                    videoRam[vy + vx + 2] = egaColors[egaColor, 0];
+                    _videoRam[vy + vx + 0] = EgaColors[egaColor, 2];
+                    _videoRam[vy + vx + 1] = EgaColors[egaColor, 1];
+                    _videoRam[vy + vx + 2] = EgaColors[egaColor, 0];
                 }
             }
 
             Display.Update();
         }
 
-        static void SetVidPixel(int x, int y, int egaColor)
+        private static void SetVidPixel(int x, int y, int egaColor)
         {
-            videoRam[(y * scanLineWidth) + (x * 3) + 0] = egaColors[egaColor, 2];
-            videoRam[(y * scanLineWidth) + (x * 3) + 1] = egaColors[egaColor, 1];
-            videoRam[(y * scanLineWidth) + (x * 3) + 2] = egaColors[egaColor, 0];
+            _videoRam[(y * ScanLineWidth) + (x * 3) + 0] = EgaColors[egaColor, 2];
+            _videoRam[(y * ScanLineWidth) + (x * 3) + 1] = EgaColors[egaColor, 1];
+            _videoRam[(y * ScanLineWidth) + (x * 3) + 2] = EgaColors[egaColor, 0];
         }
 
         static int noUpdateCount;
@@ -122,70 +121,61 @@ namespace Classes
             Update();
         }
 
-        static public void Update()
+        public static void Update()
         {
-            if (noUpdateCount == 0)
-            {
-                RawCopy(videoRam, videoRamSize);
+            if (noUpdateCount != 0) return;
 
-                if (updateCallback != null)
-                {
-                    updateCallback.Invoke();
-                }
-            }
+            RawCopy(_videoRam, VideoRamSize);
+
+            _updateCallback?.Invoke();
         }
 
-        static public void ForceUpdate()
+        public static void ForceUpdate()
         {
-            RawCopy(videoRam, videoRamSize);
+            RawCopy(_videoRam, VideoRamSize);
 
-            if (updateCallback != null)
-            {
-                updateCallback.Invoke();
-            }
+            _updateCallback?.Invoke();
         }
 
         public static void SaveVidRam()
         {
-            videoRamBkUp = (byte[])videoRam.Clone();
+            _videoRamBkUp = (byte[]) _videoRam.Clone();
         }
 
         public static void RestoreVidRam()
         {
-            videoRam = videoRamBkUp;
+            _videoRam = _videoRamBkUp;
         }
 
         public static byte GetPixel(int x, int y)
         {
-            return (byte)ram[y, x];
+            return (byte) Ram[y, x];
         }
 
         public static void SetPixel3(int x, int y, int value)
         {
             if (value < 16)
             {
-                ram[y, x] = value;
+                Ram[y, x] = value;
 
-                SetVidPixel(x, y, ram[y, x]);
+                SetVidPixel(x, y, Ram[y, x]);
             }
+
             if (value > 16)
             {
             }
         }
 
 
-      
-        public static void RawCopy(byte[] videoRam, int videoRamSize)
+        private static void RawCopy(byte[] videoRam, int videoRamSize)
         {
-            System.Drawing.Imaging.BitmapData bmpData =
-                bm.LockBits(rect, System.Drawing.Imaging.ImageLockMode.WriteOnly,
-                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            var bmpData = Bitmap.LockBits(Rect, ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
 
-            IntPtr ptr = bmpData.Scan0;
+            var ptr = bmpData.Scan0;
 
             System.Runtime.InteropServices.Marshal.Copy(videoRam, 0, ptr, videoRamSize);
 
-            bm.UnlockBits(bmpData);
+            Bitmap.UnlockBits(bmpData);
         }
     }
 }
